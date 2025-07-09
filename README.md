411f6626d237a6d57975cbdf52ac7ed7a8cc0c86
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-07-09 00:27:13 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 🔧 Flutter Commands That Generate Files
+
+```
+flutter pub get
+```
+
+```
+flutter create .
+```
+
+```
+flutter run
+```
---
+++ android/app/src/main/kotlin/com/example/constructora/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.constructora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
