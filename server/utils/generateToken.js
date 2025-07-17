44fe951e3236da36778ac3df37d8c1b773f909fe
Luis Hidalgo <hidalgovazquezluisangel2004@gmail.com>
2025-07-17 17:05:54 -0600

    Delete server directory

--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, 'constructora_jwt_secret_2024', {
-    expiresIn: '30d',
-  });
-};
-
-module.exports = generateToken;
